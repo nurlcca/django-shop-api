ef2c048947d81ab336bcaacb107686af7c3e55b2
nurlcca <lh52174@truman.edu>
2026-05-08 19:48:11 +0600

    hw1

---
+++ product/views.py
@@ -0,0 +1,46 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Category, Product, Review
+from .serializers import CategorySerializer, ProductSerializer, ReviewSerializer
+
+
+@api_view(['GET'])
+def category_list_api_view(request):
+    categories = Category.objects.all()
+    serializer = CategorySerializer(categories, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def category_detail_api_view(request, id):
+    category = Category.objects.get(id=id)
+    serializer = CategorySerializer(category)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def product_list_api_view(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def product_detail_api_view(request, id):
+    product = Product.objects.get(id=id)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def review_list_api_view(request):
+    reviews = Review.objects.all()
+    serializer = ReviewSerializer(reviews, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def review_detail_api_view(request, id):
+    review = Review.objects.get(id=id)
+    serializer = ReviewSerializer(review)
+    return Response(serializer.data)
